788f01919e691332e7eb218d09e81d6dd7c6b64f
Armann-Sch <armannsch@gmail.com>
2025-04-18 18:33:39 +0000

    Added functionality and commenting.

--- gloveanalyzer.py
+++ gloveanalyzer.py
@@ -1,31 +1,71 @@
+# Libraries used
 import numpy as np
+from scipy import spatial
 
+# Global variables declared
+modelA = {}
+modelB = {}
+
+distsA = {}
+distsB = {}
+
+# Accepts a filename as an argument and reads the file contents
+# Glove vectors are assumed to have a dimension of 300
 def load_glove_model(File):
     glove_model = {}
+
     with open(File, 'r') as f:
         for line in f:
             split_line = line.split()
+            
+            # Some tokens came out as two words sepearated by a space so
+            # the systems checks to ensure it does not try to convert a
+            # string to a float
             if (len(split_line) == 302):
                 word = split_line[0] + " " + split_line[1]
                 embedding = np.array(split_line[2:], dtype=np.float64)
             else:
                 word = split_line[0]
                 embedding = np.array(split_line[1:], dtype=np.float64)
+        
             glove_model[word] = embedding
+        
         print(f"{len(glove_model)} words loaded!")
         return glove_model
 
+def prepare_models(A, B):
+    modelA = load_glove_model(A)
+    modelB = load_glove_model(B)
+
+prepare_models("gloves/sherlock/vectors.txt", "gloves/14/model.txt")
+# Accepts a model object and a word
+# Returns a sorted lsit of words
+# paired with their euclid distances
+# from the given word
+def make_sorted(model, word):
+    sorted = {}
+
+    return sorted
+
+# Takes a model, main word string and integer n
+# Returns the n closest words in terms of euclid
+# distance from the main word along with the distance
 def get_n_closest(model, main_word, n):
     closest_words = []
     # Search for n closest vectors
 
     return closest_words
 
+# Takes a model, main word, float r as arguments
+# Returns all the words with a euclid distance
+# equal to or less than the provided range
+# along with the distances
 def get_within_range(model, main_word, r):
     in_range = []
     #search for words within range r
 
     return in_range
+
 
 def get_distance(vecA, vecB):
     s = 0
@@ -33,21 +73,63 @@ def get_distance(vecA, vecB):
         s += np.square(vecA[i]-vecB[i])
     return np.sqrt(s)
 
-args = input().split(" ")
-inA = args[0]
-inB = args[1]
+
+def compare_word(word, wr):
+    #sortedA(modelA.keys())
+
+
+    # wr determines whether results will be written over a file, appended to a file or printed int the terminal
+    return word
+
+#args = input().split(" ")
+#inA = args[0]
+#inB = args[1]
 
 #modelA = load_glove_model(inA)
 #modelB = load_glove_model(inB)
 conversion_rules = []
 
-if len(args) > 2:
-    inC = args[2]
+#if len(args) > 2:
+#    inC = args[2]
 
 
-modelA = load_glove_model("gloves/sherlock/vectors.txt")
-modelB = load_glove_model("gloves/14/model.txt")
-print(modelA['Holmes'])
-print(modelB['Holmes'])
-print(modelA['Holmes']-modelB['Holmes'])
-print(get_distance(modelA['Holmes'], modelB['Holmes']))
+
+#print(modelA['Holmes']-modelB['Holmes'])
+#print(get_distance(modelA['Holmes'], modelB['Holmes']))
+
+commands = {
+    'help': ['h', 'help', 'commands'],
+    'quit': ['q', 'quit', 'exit'],
+    'compare': ['c', 'comp', 'compare'],
+    'load': ['load'],
+    'loadA': ['loada', 'la'],
+    'loadB': ['loadb', 'lb'],
+    'sizes': ['sizes'],
+    'sizeA': ['sizeA'],
+    'sizeB': ['sizeB'],
+}
+
+commands_desc = {
+    'help': 'List'
+}
+
+programname = "GloveAnalyzer"
+print(f"Welcome to ${programname}!\nInsert h for a list of commands")
+run = True
+while run:
+    args = input("What would you like to do? ").split(" ")
+    if len(args) != 0:
+        args[0] = args[0].lower()
+        if args[0].lower() in commands['quit']:
+            run = False
+        elif args[0].lower() in commands['compare']:
+            print("comparing")
+        elif args[0] in commands['load']:
+            dists = {}
+            if args[1].lower() == "a":
+                modelA = load_glove_model(args[2])
+            elif args[1].lower() == "b":
+                modelB = load_glove_model(args[2])
+            else:
+                modelA = load_glove_model(args[1])
+                modelB = load_glove_model(args[2])
